0db1f2a03925b74f8ba8d003363c2fd81fc04361
Danco Nawm <danconawm@gmail.com>
2022-04-04 06:27:53 +0300

    Mon 4th April 2022

---
+++ eTickets/Models/Actor_Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Models
+{
+    public class Actor_Movie
+    {//Joining relationship table for one is to many 
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+
+        public int ActorId { get; set; }
+
+        public Actor Actor { get; set; }
+    }
+}
